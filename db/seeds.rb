b30fc820ca7300432ae3d35d834a285ea5539e2a
Axel Abildtrup <axabildtrup@hotmail.com>
2019-11-06 09:11:53 +0100

    Challenge completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Article.destroy_all
+
+puts "Creating 10 fake articles"
+
+10.times do
+  title = Faker::DcComics.title
+  content = Faker::Lorem.paragraph(sentence_count: 2, supplemental: false, random_sentences_to_add: 50)
+
+  article = Article.new(title: title, content: content)
+  article.save!
+end
+
+puts "Done creating 10 fake articles"
+
